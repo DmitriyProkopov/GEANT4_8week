21afafa7f656d7fe8ccf3ceff018eba3d518707c
DmitriyProkopov <dimonprokopov2003@gmail.com>
2024-10-30 22:04:24 +0300

    Add files via upload

---
+++ daniel.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+void klubnichka(int N, std::ifstream *FN)
+{
+	std::string word;
+	for(int i = 0;i < N;i++)
+	{
+	getline(*FN,word);
+	}
+}
